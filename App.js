292697708a09d6d6529b35109ee2f12e3d8b5f71
BanuDanabakiyam <banubala9655@gmail.com>
2023-10-13 23:02:50 +0530

    final Change

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ document.getElementById("name").innerHTML = "Name :";
 document.getElementById("email").innerHTML = "E-mail :";
 document.getElementById("phone").innerHTML = "Phone :";
 document.getElementById("resume").innerHTML = "Resume :";
+function display(){
+    let text = document.getElementById("submit").innerText;
+    document.getElementById("message").innerHTML = "Submitted Succesfully!";
+ 
+}
+
 
 
 
