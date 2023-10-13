69bd8aa923f2db41a34e9794ac3bcfcdda43b00a
BanuDanabakiyam <banubala9655@gmail.com>
2023-10-13 22:35:42 +0530

    final

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-document.getElementById("Heading").innerHTML = "RESUME SUBMISSION FORM"
+document.getElementById("Heading").innerHTML ="RESUME SUBMISSION FORM";
+document.getElementById("name").innerHTML = "Name :";
+document.getElementById("email").innerHTML = "E-mail :";
+document.getElementById("phone").innerHTML = "Phone :";
+document.getElementById("resume").innerHTML = "Resume :";
+
+
+
